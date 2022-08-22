7c2ad18c927aaaa828bf3aeb64bf058e9a5f6e9a
Saky <souhoc@gmail.com>
2022-08-22 13:48:09 +0200

    test on 42's pc

--- srcs/main.c
+++ srcs/main.c
@@ -3,29 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nfelsemb <nfelsemb@student.42.fr>          +#+  +:+       +#+        */
+/*   By: shocquen <shocquen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 20:59:55 by nfelsemb          #+#    #+#             */
-/*   Updated: 2022/06/21 14:13:40 by nfelsemb         ###   ########.fr       */
+/*   Updated: 2022/08/22 13:46:10 by shocquen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void parser(char *input, t_env *enviro)
+{
+	t_list	*tokens;
+	(void)enviro;
+	if (quote_checker(input) == -1)
+		fprintf(stderr, "QUOTE_ERROR\n");
+	tokens = tokens_split(input);
+	while ((tokens)->content)
+	{
+		printf("token: |%s|\n", ((t_token *)(tokens)->content)->value);
+		tokens = (tokens)->next;
+	}
+}
+
 void	parsing(char *cmd, t_env *enviro)
 {
-	char	**retsplit;
+	// char	*print;
 
-	if (cmd == NULL)
+	// print = NULL;
+	parser(cmd, enviro);
+	/* if (cmd == NULL)
 		exitfree(enviro);
-	retsplit = ft_split(cmd, '|');
-	if (retsplit[1])
-	{
-		piped(retsplit, enviro);
-		return ;
-	}
-	freetab(retsplit);
-	if (ft_strncmp(cmd, "exit", 4) == 0)
+	else if (ft_strncmp(cmd, "exit", 4) == 0)
 	{
 		free(cmd);
 		exitfree(enviro);
@@ -60,8 +69,7 @@ void	parsing(char *cmd, t_env *enviro)
 		exed(cmd, enviro);
 	}
 	else
-		lexe(cmd, enviro);
-	free(cmd);
+		lexe(cmd, enviro); */
 }
 
 void	ctrlc(int i)
@@ -69,7 +77,7 @@ void	ctrlc(int i)
 	signal(SIGINT, ctrlc);
 	i = i + 5;
 	rl_on_new_line();
-	rl_replace_line("", 1);
+	// rl_replace_line("", 1);
 	printf("\n");
 	rl_redisplay();
 }
@@ -90,6 +98,7 @@ int	main(int ac, char **av, char **env)
 		if (cmd && ft_strlen(cmd) > 0)
 			add_history(cmd);
 		parsing(cmd, enviro);
+		free(cmd);
 	}
 	return (0);
 }
