ed1ad818a00ddff6fe52030a7a6869cdc6403d01
Saky <souhoc@gmail.com>
2022-08-26 18:56:43 +0200

    refactor(MAIN_C): clean main

--- srcs/main.c
+++ srcs/main.c
@@ -6,19 +6,26 @@
 /*   By: shocquen <shocquen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 20:59:55 by nfelsemb          #+#    #+#             */
-/*   Updated: 2022/08/26 01:51:01 by shocquen         ###   ########.fr       */
+/*   Updated: 2022/08/26 18:56:07 by shocquen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void parser(char *input, t_env *enviro)
+void	parser(char *input, t_env *enviro)
 {
 	t_list	*tokens;
+	t_list	*start;
+
 	(void)enviro;
 	if (quote_checker(input) == -1)
-		fprintf(stderr, "QUOTE_ERROR\n");
-	tokens = build_tokens(input);
+		fprintf(stderr, RED"QUOTE_ERROR\n"RESET);
+	if (sk_strcspn(input, ";\\") < ft_strlen(input))
+		fprintf(stderr, RED"SPECIAL_CHAR_ERROR\n"RESET);
+	// todo: exit if error
+	tokens = NULL;
+	build_tokens(&tokens, input);
+	start = tokens;
 	while (tokens)
 	{
 		printf("\ttoken->value: "YELLOW"%s"RESET"\n\ttoken->type: %d\n",
@@ -26,6 +33,7 @@ void parser(char *input, t_env *enviro)
 			((t_token *)tokens->content)->type);
 		tokens = tokens->next;
 	}
+	free(start);
 }
 
 void	ctrlc(int i)
@@ -33,7 +41,7 @@ void	ctrlc(int i)
 	signal(SIGINT, ctrlc);
 	i = i + 5;
 	rl_on_new_line();
-	// rl_replace_line("", 1);
+	rl_replace_line("", 1);
 	printf("\n");
 	rl_redisplay();
 }
